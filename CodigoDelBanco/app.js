85156f989beebba6766937d7715ac0432de247d2
Miguel vargas Navarro <mvargasnavarro19@gmail.com>
2024-11-08 11:47:34 -0500

    Added Libros-Electronicos-SENA-By-Miguel-AVN as a submodule

---
+++ CodigoDelBanco/app.js
@@ -0,0 +1,25 @@
+//Proywcto Banco
+
+let registro = (cedula, nombre, pin) =>{
+    
+    user = {
+        id: cedula,
+        username: nombre,
+        key: pin,
+    };
+    
+    return
+    
+} ;
+
+let users = [
+    user={}
+];
+
+
+registro(1231, "Miguel", "1234");
+
+
+console.log(user.push);
+
+
